7b00e3d3ab2fa089f097120b80cbcd13280b0bff
David Watson <david.w@tson.net.au>
2023-06-23 12:21:35 +1000

    script to setup flux config

---
+++ manifests/fluxsetup.sh
@@ -0,0 +1,18 @@
+CLSUTER=aksvanilla
+RG=aksvanilla
+
+az extension add -n k8s-configuration
+az extension add -n k8s-extension
+
+az extension update -n k8s-configuration
+az extension update -n k8s-extension
+
+az k8s-configuration flux create -g $RG -c $CLSUTER \
+-n broker \
+--namespace broker-flux \
+-t maangedClusters \
+--scope cluster \
+--interval 1m0s \
+-u https://github.com/davidxw/video-broker-lite \
+--branch main  \
+--kustomization name=broker-helm path=./manifests/releases sync_interval=1m0s prune=true 
