4051fed7dce104b9c30addf6fdf566dff87e32a1
David Watson <david.w@tson.net.au>
2023-06-29 16:17:26 +1000

    Change to private repository

---
+++ manifests/releases/installnginx.sh
@@ -0,0 +1,8 @@
+NAMESPACE=nginx
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+ 
+ helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace $NAMESPACE \
+   --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz \
+   --set controller.service.externalTrafficPolicy=Local
